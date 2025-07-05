0e33442d5969a19f45541dbf5aa0fc39998c60e2
Amin Rnx <amin.raji96@hotmail.com>
2025-07-05 20:41:52 +0330

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,7 @@
+"""
+Preprocessing Pipeline (PP)
+File name:		preprocessing.py
+Description:	A preprocessing pipeline to make data, model-ready.
+Created:		5th/JULY/2025
+Author:			nerveinvader
+"""
